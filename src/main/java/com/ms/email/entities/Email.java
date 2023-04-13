9085dd8c901294c73b0cf911c77ff5caf7b9f4ef
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-04-13 10:18:08 -0300

    Entidade email, emaildto e statusemail adicionados

--- src/main/java/com/ms/email/entities/Email.java
+++ src/main/java/com/ms/email/entities/Email.java
@@ -1,4 +1,29 @@
 package com.ms.email.entities;
 
-public class Email {
+import com.ms.email.enums.StatusEmail;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "TB_EMAIL")
+public class Email implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long emailId;
+    private String ownerRef;
+    private String emailFrom;
+    private String emailTo;
+    private String subject;
+    @Column(columnDefinition = "TEXT")
+    private String text;
+    private LocalDateTime sendDateEmail;
+    private StatusEmail statusEmail;
+
 }
