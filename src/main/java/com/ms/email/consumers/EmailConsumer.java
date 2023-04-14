a928396c1506331c856f50e0232d61249278ddbe
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-04-14 13:11:24 -0300

    Microsserviço de email com rabbitmq implementado

--- src/main/java/com/ms/email/consumers/EmailConsumer.java
+++ src/main/java/com/ms/email/consumers/EmailConsumer.java
@@ -1,4 +1,29 @@
 package com.ms.email.consumers;
 
+import com.ms.email.dtos.EmailDto;
+import com.ms.email.entities.Email;
+import com.ms.email.services.EmailService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
 public class EmailConsumer {
+
+    @Autowired
+    EmailService emailService;
+
+    @RabbitListener(queues = "${spring.rabbitmq.queue}")
+    public void listen(@Payload EmailDto emailDto){
+        Email email = new Email();
+        BeanUtils.copyProperties(emailDto,email);
+        emailService.sendEmail(email);
+        System.out.println("Email Status: " + email.getStatusEmail().toString());
+    }
+
+
 }
